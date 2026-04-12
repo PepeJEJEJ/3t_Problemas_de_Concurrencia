c371cb242bc12b5672c0d652164caa6a61c99dcf
PepeJEJEJ <jah0005@alu.medac.es>
2026-04-12 12:51:48 +0200

    Update BD2.sql

--- BD2.sql
+++ BD2.sql
@@ -13,5 +13,16 @@ insert into cuentas values (2,'Luis',500.00);
 
 start transaction;
 select * from cuentas;
-update cuentas set saldo=saldo-100 where id=1;
-update cuentas set saldo=saldo+100 where id=2;
+update cuentas set saldo = saldo - 100 where id = 1;
+-- MENOS SALDO
+update cuentas set saldo = saldo + 100 where id = 2;
+-- MAS SALDO
+start transaction;
+-- EMPEZAR OTRA TRANSACCION
+update cuentas set saldo = 2000;
+-- ALTERAR EL VALOR DEL SALDO
+select * from cuentas;
+-- VER LOS DATOS
+savepoint sp1;-- PUNTO DE GUARDADO
+update cuentas set saldo = saldo - 100 where id = 1;
+-- Actualizar pa ir al savepoint
